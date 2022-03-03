a0295232be121b9f8a64a862f2c5ce97cc0cbb05
Alexander Barth <barth.alexander@gmail.com>
2022-03-03 21:52:38 +0100

    NetCDF_jll build 400.802.102+0

---
+++ src/NetCDF_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule NetCDF_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("NetCDF")
+JLLWrappers.@generate_main_file("NetCDF", UUID("7243133f-43d8-5620-bbf4-c2c921802cf3"))
+end  # module NetCDF_jll
