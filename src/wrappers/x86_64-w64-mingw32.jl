1195e8ccee47f2327db197514cfe33aec87185f7
Alexander Barth <barth.alexander@gmail.com>
2022-05-03 13:41:58 +0200

    NetCDF_jll build 400.802.106+0

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -5,12 +5,12 @@ using HDF5_jll
 using Zlib_jll
 using LibCURL_jll
 JLLWrappers.@generate_wrapper_header("NetCDF")
-JLLWrappers.@declare_library_product(libnetcdf, "libnetcdf-19.dll")
+JLLWrappers.@declare_library_product(libnetcdf, "libnetcdf.dll")
 function __init__()
     JLLWrappers.@generate_init_header(HDF5_jll, Zlib_jll, LibCURL_jll)
     JLLWrappers.@init_library_product(
         libnetcdf,
-        "bin\\libnetcdf-19.dll",
+        "bin\\libnetcdf.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
