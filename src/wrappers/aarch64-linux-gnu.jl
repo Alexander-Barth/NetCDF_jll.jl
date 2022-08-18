727c9564a994dca5a6a3cce9a67c68661c7d4228
Alexander Barth <barth.alexander@gmail.com>
2022-08-18 09:10:22 +0200

    NetCDF_jll build 400.902.23+0

--- src/wrappers/aarch64-linux-gnu.jl
+++ src/wrappers/aarch64-linux-gnu.jl
@@ -3,13 +3,12 @@ export libnetcdf
 
 using HDF5_jll
 using Zlib_jll
-using Zstd_jll
 using XML2_jll
 using LibCURL_jll
 JLLWrappers.@generate_wrapper_header("NetCDF")
 JLLWrappers.@declare_library_product(libnetcdf, "libnetcdf.so.19")
 function __init__()
-    JLLWrappers.@generate_init_header(HDF5_jll, Zlib_jll, Zstd_jll, XML2_jll, LibCURL_jll)
+    JLLWrappers.@generate_init_header(HDF5_jll, Zlib_jll, XML2_jll, LibCURL_jll)
     JLLWrappers.@init_library_product(
         libnetcdf,
         "lib/libnetcdf.so",
