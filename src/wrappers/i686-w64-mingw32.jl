fe78f30977ba5044c70ca69cdb2e1c15d737e234
Alexander Barth <barth.alexander@gmail.com>
2024-01-29 15:50:50 +0100

    NetCDF_jll build 400.902.241+0

--- src/wrappers/i686-w64-mingw32.jl
+++ src/wrappers/i686-w64-mingw32.jl
@@ -9,10 +9,11 @@ using Zlib_jll
 using Zstd_jll
 using libzip_jll
 using Blosc_jll
+using OpenMPI_jll
 JLLWrappers.@generate_wrapper_header("NetCDF")
 JLLWrappers.@declare_library_product(libnetcdf, "libnetcdf-19.dll")
 function __init__()
-    JLLWrappers.@generate_init_header(Bzip2_jll, HDF5_jll, LibCURL_jll, XML2_jll, Zlib_jll, Zstd_jll, libzip_jll, Blosc_jll)
+    JLLWrappers.@generate_init_header(Bzip2_jll, HDF5_jll, LibCURL_jll, XML2_jll, Zlib_jll, Zstd_jll, libzip_jll, Blosc_jll, OpenMPI_jll)
     JLLWrappers.@init_library_product(
         libnetcdf,
         "bin\\libnetcdf-19.dll",
