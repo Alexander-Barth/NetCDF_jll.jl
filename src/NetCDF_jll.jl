541bd751780e56d5e46292687eed35cd2e8dee22
Alexander Barth <barth.alexander@gmail.com>
2025-03-26 09:51:58 +0100

    NetCDF_jll build 401.900.300+1

--- src/NetCDF_jll.jl
+++ src/NetCDF_jll.jl
@@ -2,6 +2,9 @@
 baremodule NetCDF_jll
 using Base
 using Base: UUID
+using LazyArtifacts
+using MPIPreferences
+Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("NetCDF")
